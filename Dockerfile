5a3a5e385b9b59bcbcb9433503cf02213b9f492d
Gabriel Izquierdo <gabriel.izquierdo.gonzalez@gmail.com>
2025-02-20 11:36:20 +0100

    mejorando los errores sonarqube

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,17 @@
 
 FROM python:3.10
 
+RUN useradd -m zapuser
 WORKDIR /app
+
+COPY requirements.txt requirements.txt
+RUN pip install --no-cache-dir -r requirements.txt
 
 COPY . .
 
-RUN pip install -r requirements.txt
+RUN chown -R zapuser:zapuser /app
+USER zapuser
 
-CMD ["python", "main.py"]
+EXPOSE 5000
+
+CMD ["gunicorn", "-w", "4", "-b", "0.0.0.0:5000", "app:app"]
--- app.py
+++ app.py
@@ -1,13 +1,20 @@
 from init_db import crear_tablas 
-from flask import Flask
+from flask import Flask, render_template, request
+from flask_wtf.csrf import CSRFProtect
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = os.getenv("CSRF_TOKEN")
+csrf = CSRFProtect(app)
 
 crear_tablas()
 
-@app.route("/")
+@app.route("/", methods=['GET'])
 def home():
     return "✅ API de Auditoría de Seguridad Activa"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000, debug=True)
